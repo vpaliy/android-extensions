0e33733a9f8b055a0c89191d9cd3f82c3effe053
vpaliy <vpaliy97@gmail.com>
2017-11-18 23:33:48 -0500

    update Animation extensions

--- kotlin-extensions/src/main/kotlin/com/vpaliy/kotlin_extensions/Animation.kt
+++ kotlin-extensions/src/main/kotlin/com/vpaliy/kotlin_extensions/Animation.kt
@@ -72,4 +72,61 @@ inline fun ViewPropertyAnimator.translation(factor: () -> Float)=translation(fac
 
 inline fun ViewPropertyAnimator.translationBy(factor: () -> Float)=translationBy(factor())
 
+fun Animator.onEnd(callback:(Animator?)->Unit)=Keeper(this).onEnd(callback)
+
+fun Animator.onStart(callback:(Animator?)->Unit)=Keeper(this).onStart(callback)
+
+fun Animator.onCancel(callback:(Animator?)->Unit)=Keeper(this).onCancel(callback)
+
+fun Animator.onRepeat(callback:(Animator?)->Unit)=Keeper(this).onRepeat(callback)
+
+class Keeper(private val animator:Animator){
+    private val executor=ListenerExecutor()
+
+    init { animator.addListener(executor) }
+
+    fun onEnd(callback:(Animator?) -> Unit)=apply {
+        executor.end=callback
+    }
+
+    fun onStart(callback: (Animator?) -> Unit)=apply {
+        executor.start=callback
+    }
+
+    fun onCancel(callback: (Animator?) -> Unit)=apply {
+        executor.cancel=callback
+    }
+
+    fun onRepeat(callback: (Animator?) -> Unit)=apply {
+        executor.repeat=callback
+    }
+
+    fun animator()=animator
+}
+
+
+private class ListenerExecutor:Animator.AnimatorListener {
+    var end:((Animator?) -> Unit)? = null
+    var start: ((Animator?) ->Unit)? = null
+    var cancel:((Animator?)->Unit)?=null
+    var repeat:((Animator?)->Unit)?=null
+
+    override fun onAnimationCancel(animation: Animator?) {
+        cancel?.invoke(animation)
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+        end?.invoke(animation)
+    }
+
+    override fun onAnimationRepeat(animation: Animator?) {
+        repeat?.invoke(animation)
+    }
+
+    override fun onAnimationStart(animation: Animator?) {
+        start?.invoke(animation)
+    }
+}
+
+
 
